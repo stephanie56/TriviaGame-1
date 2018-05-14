e88558a29b05c63d6855be1eca325593c2c830d8
Stephanie Zeng <stephanie56@users.noreply.github.com>
2018-05-14 14:41:31 -0400

    Added reviews on code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,6 +26,7 @@
 
 var points = 0;
 
+//
 var questionOne = $("#q1a");
 var questionTwo = $("#q2a");
 var questionThree = $("#q3a");
@@ -36,11 +37,20 @@ var questionSix = $("#q6a");
 var questionLoop = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix];
 
 $("#7aa").on("click", function() {
-    for (var i = 0; i < questionLoop.length; i++) {
-        console.log(i)
-        console.log(questionLoop[i])
-    }
+//     for (var i = 0; i < questionLoop.length; i++) {
+//         console.log(i)
+//         console.log(questionLoop[i])
+//     }
+
+    /** HINTS **/
+    // Step1: Get value of the selected radio button (e.g. the selected radio button with a name of #q1a in this example )
+    var answer1 = $( "input[name=q1a]:checked" ).val();
     
+    // Step2: iterate each answer
+    // userAnswers = [answer1, answer2 ... answer5];
+    for (var i = 0; i < userAnswers.length; i++) {
+        // compared each user answer with true; if true, points++
+    }    
 });
      
     var postScore = Math.round((points/6)*100); 
